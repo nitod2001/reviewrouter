fe59f5e298f517d5a2ff60cf807bf60f73ccc492
Tin <dotinvta2@gmail.com>
2022-12-01 21:26:17 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./component/Home";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Panner from "./component/Panner";
 import Page1 from "./component/Page1";
 import Page2 from "./component/Page2";
 
 function App() {
+  const location = useLocation();
+  console.log(location);
+  const changePath = () => {
+    if (location.pathname === "/") {
+      location.pathname = "/football";
+    } else location.pathname = "/football";
+    return location.pathname;
+  };
   return (
     <div className="App">
       <Routes>
-        <Route path="/football" element={<Home></Home>}></Route>
+        <Route path={changePath()} element={<Home></Home>}></Route>
         <Route
           path="/football/Page1"
           element={<Page1 name="messi"></Page1>}
